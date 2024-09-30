648a05ddc19c520653f930e01659a6dbe451a9cd
Adriel Velazquez <adrielvelazquez@gmail.com>
2024-09-30 12:42:50 -0700

    Does this work

--- hosts/default/configuration.nix
+++ hosts/default/configuration.nix
@@ -8,6 +8,7 @@
   imports =
     [ # Include the results of the hardware scan.
       ./hardware-configuration.nix
+      ./razer-blade-14-2023.nix
       inputs.home-manager.nixosModules.home-manager
 
     ];
