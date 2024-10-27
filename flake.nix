75c6eba01ebd7b0b40cfd1cc1191aa35b05c85f6
Adriel Velazquez <adrielvelazquez@gmail.com>
2024-10-27 00:28:17 -0700

    Adding unstable as the default channel

--- flake.nix
+++ flake.nix
@@ -3,13 +3,6 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-    nixvim = {
-        url = "github:nix-community/nixvim";
-        # If you are not running an unstable channel of nixpkgs, select the corresponding branch of nixvim.
-        # url = "github:nix-community/nixvim/nixos-24.05";
-
-        inputs.nixpkgs.follows = "nixpkgs";
-    };
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
