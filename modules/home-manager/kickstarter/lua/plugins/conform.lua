305f5d2bffbaa997c310f6cb46086ad1d844ae1a
Adriel Velazquez <adrielvelazquez@gmail.com>
2024-12-12 12:54:47 -0800

    Adding conform back into the neovim config

--- modules/home-manager/kickstarter/lua/plugins/conform.lua
+++ modules/home-manager/kickstarter/lua/plugins/conform.lua
@@ -1,12 +1,19 @@
-return 
-{
+return {
   "stevearc/conform.nvim",
   config = function()
-    require("conform").setup({
+    local conform = require("conform")
+    conform.setup({
       formatters_by_ft = {
         go = { "gofumpt" },
         nix = { "nixfmt" },
       },
+    })
+
+    vim.api.nvim_create_autocmd("BufWritePre", {
+      pattern = "*.go",
+      callback = function()
+        conform.format({ async = false, lsp_fallback = true })
+      end,
     })
   end,
 }
