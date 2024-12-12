2771e42ab4a39af348122dd5e91977db7dccb5f5
Adriel Velazquez <adrielvelazquez@gmail.com>
2024-12-12 12:26:31 -0800

    removing the lazyvim config now that I have my own

--- modules/home-manager/nvim/lua/config/keymaps.lua
+++
@@ -1,3 +0,0 @@
-local builtin = require("telescope.builtin")
-vim.keymap.set("n", "<leader>fg", builtin.live_grep, { desc = "Live Grep on all files and content" })
-vim.keymap.set("n", "<leader>fh", builtin.help_tags, { desc = "fzf on help tags" })
