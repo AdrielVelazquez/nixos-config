b273ee0b559d215149d347f8c9b58276b0634239
Adriel Velazquez <adrielvelazquez@gmail.com>
2024-12-14 22:21:59 -0800

    Intergration for the colorschemes

--- modules/home-manager/nvim/lua/plugins/colorscheme.lua
+++ modules/home-manager/nvim/lua/plugins/colorscheme.lua
@@ -6,6 +6,46 @@ return {
   -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
   -- 'catppuccin/nvim',
   "rose-pine/neovim",
+  name = "rose-pine",
+  opts = {
+    integrations = {
+      aerial = true,
+      alpha = true,
+      cmp = true,
+      dashboard = true,
+      flash = true,
+      grug_far = true,
+      gitsigns = true,
+      headlines = true,
+      illuminate = true,
+      indent_blankline = { enabled = true },
+      leap = true,
+      lsp_trouble = true,
+      mason = true,
+      markdown = true,
+      mini = true,
+      native_lsp = {
+        enabled = true,
+        underlines = {
+          errors = { "undercurl" },
+          hints = { "undercurl" },
+          warnings = { "undercurl" },
+          information = { "undercurl" },
+        },
+      },
+      navic = { enabled = true, custom_bg = "lualine" },
+      neotest = true,
+      neotree = true,
+      noice = true,
+      notify = true,
+      semantic_tokens = true,
+      snacks = true,
+      telescope = true,
+      treesitter = true,
+      treesitter_context = true,
+      which_key = true,
+    },
+  },
   priority = 1000, -- Make sure to load this before all the other start plugins.
   init = function()
     -- Load the colorscheme here.
