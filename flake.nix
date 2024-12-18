c70f43f20910098fcce0c554332cb921defef890
Adriel Velazquez <adrielvelazquez@gmail.com>
2024-12-18 00:29:20 -0800

    Better Flake for specific packages

--- flake.nix
+++ flake.nix
@@ -23,62 +23,132 @@
       ...
     }@inputs:
     let
-      systems = {
-        linux = "x86_64-linux";
-        mac = "aarch64-darwin";
-      };
-      # system = "x86_64-linux";
-      # pkgs = nixpkgs.legacyPackages.${system};
-      pkgsForSystem =
-        system:
-        import nixpkgs {
+      mkNixosConfig =
+        system: extraModules:
+        nixpkgs.lib.nixosSystem {
           inherit system;
+          specialArgs = { inherit inputs system; };
+          modules = extraModules ++ [
+            { nixpkgs.overlays = [ reddit.overlay ]; }
+          ];
+        };
+
+      mkDarwinConfig =
+        system: extraModules:
+        nix-darwin.lib.darwinSystem {
+          inherit system;
+          specialArgs = { inherit inputs system; };
+          modules = extraModules ++ [
+            { nixpkgs.overlays = [ reddit.overlay ]; }
+          ];
+        };
+
+      mkHmConfig =
+        pkgs: extraModules:
+        home-manager.lib.homeManagerConfiguration {
+          inherit pkgs;
+          modules = extraModules;
         };
     in
     {
       nixosConfigurations = {
-        razer14 = nixpkgs.lib.nixosSystem {
-          system = systems.linux;
-          specialArgs = {
-            inherit inputs;
-          };
-          modules = [
-            ./hosts/razer14/configuration.nix
-          ];
-        };
+        razer14 = mkNixosConfig "x86_64-linux" [ ./hosts/razer14/configuration.nix ];
+        dell = mkNixosConfig "x86_64-linux" [ ./hosts/dell-plex-server/configuration.nix ];
+      };
 
-        dell = nixpkgs.lib.nixosSystem {
-          system = systems.linux;
-          specialArgs = {
-            inherit inputs;
-          };
-          modules = [
-            ./hosts/dell-plex-server/configuration.nix
-          ];
-        };
-      };
-      homeConfigurations = {
-        adriel = home-manager.lib.homeManagerConfiguration {
-          pkgs = pkgsForSystem systems.linux;
-          modules = [
-            ./users/adriel.nix
-          ];
-        };
-      };
       darwinConfigurations = {
-        PNH46YXX3Y = nix-darwin.lib.darwinSystem {
-          system = systems.mac;
-          modules = [
-            { nixpkgs.overlays = [ reddit.overlay ]; }
-            ./hosts/reddit-mac/configuration.nix
-            home-manager.darwinModules.home-manager
-            {
-              home-manager.useGlobalPkgs = true;
-              home-manager.useUserPackages = true;
-              home-manager.users."adriel.velazquez" = import ./users/adriel.velazquez.nix;
-            }
-          ];
-        };
+        PNH46YXX3Y = mkDarwinConfig "aarch64-darwin" [
+          ./hosts/reddit-mac/configuration.nix
+          home-manager.darwinModules.home-manager
+          {
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+            home-manager.users."adriel.velazquez" = {
+              imports = [ ./users/adriel.velazquez.nix ];
+            };
+          }
+        ];
+      };
+
+      homeConfigurations = {
+        adriel = mkHmConfig (nixpkgs.legacyPackages."x86_64-linux") [
+          ./users/adriel.nix
+        ];
       };
     };
 }
+#{
+#   description = "Nixos config flake";
+#
+#   inputs = {
+#     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+#     nix-darwin.url = "github:LnL7/nix-darwin";
+#     nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
+#     reddit.url = "git+ssh://git@github.snooguts.net/reddit/reddit-nix.git";
+#     reddit.inputs.nixpkgs.follows = "nixpkgs";
+#     home-manager = {
+#       url = "github:nix-community/home-manager/master";
+#       inputs.nixpkgs.follows = "nixpkgs";
+#     };
+#   };
+#
+#   outputs =
+#     {
+#       self,
+#       nixpkgs,
+#       home-manager,
+#       nix-darwin,
+#       reddit,
+#       ...
+#     }@inputs:
+#     let
+#       system = "x86_64-linux";
+#       pkgs = nixpkgs.legacyPackages.${system};
+#     in
+#     {
+#       nixosConfigurations = {
+#         razer14 = nixpkgs.lib.nixosSystem {
+#           inherit system;
+#           specialArgs = {
+#             inherit inputs system;
+#           };
+#           modules = [
+#             ./hosts/razer14/configuration.nix
+#           ];
+#         };
+#
+#         dell = nixpkgs.lib.nixosSystem {
+#           inherit system;
+#           specialArgs = {
+#             inherit inputs system;
+#           };
+#           modules = [
+#             ./hosts/dell-plex-server/configuration.nix
+#           ];
+#         };
+#       };
+#       homeConfigurations = {
+#         adriel = home-manager.lib.homeManagerConfiguration {
+#           inherit pkgs;
+#           modules = [
+#             ./users/adriel.nix
+#             # inputs.nixvim.homeManagerModules.nixvim
+#           ];
+#         };
+#       };
+#       darwinConfigurations = {
+#         PNH46YXX3Y = nix-darwin.lib.darwinSystem {
+#           modules = [
+#             { nixpkgs.overlays = [ reddit.overlay ]; }
+#             ./hosts/reddit-mac/configuration.nix
+#             home-manager.darwinModules.home-manager
+#             {
+#               home-manager.useGlobalPkgs = true;
+#               home-manager.useUserPackages = true;
+#               home-manager.users."adriel.velazquez" = import ./users/adriel.velazquez.nix;
+#             }
+#           ];
+#         };
+#       };
+#     };
+# }
