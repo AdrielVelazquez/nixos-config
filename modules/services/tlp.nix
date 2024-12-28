8ce25cae530ab1c58007461d9faf97162f16b2a3
Adriel Velazquez <adrielvelazquez@gmail.com>
2024-12-28 01:10:27 -0800

    changing on battery performance

--- modules/services/tlp.nix
+++ modules/services/tlp.nix
@@ -19,11 +19,6 @@ in
         CPU_ENERGY_PERF_POLICY_ON_BAT = "power";
         CPU_ENERGY_PERF_POLICY_ON_AC = "performance";
 
-        CPU_MIN_PERF_ON_AC = 0;
-        CPU_MAX_PERF_ON_AC = 100;
-        CPU_MIN_PERF_ON_BAT = 0;
-        CPU_MAX_PERF_ON_BAT = 90;
-
         CPU_BOOST_ON_AC = 1;
         CPU_BOOST_ON_BAT = 0;
 
@@ -34,7 +29,6 @@ in
         AMDGPU_ABM_LEVEL_ON_BAT = 3;
 
         RADEON_DPM_STATE_ON_BAT = "battery";
-
       };
     };
   };
