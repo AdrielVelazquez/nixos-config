9d586de135811325b87f62a3f0aeceeb7b0bec05
Adriel Velazquez <adrielvelazquez@gmail.com>
2025-01-06 10:27:55 -0800

    removing the dependency of nvim-cmp

--- users/adriel.velazquez.nix
+++ users/adriel.velazquez.nix
@@ -40,8 +40,10 @@
     pkgs.gh
     pkgs.nerd-fonts.bigblue-terminal
     pkgs.nerd-fonts.victor-mono
-    pkgs.nerd-fonts.mononoki
     pkgs.nerd-fonts.zed-mono
+    pkgs.nerd-fonts.mononoki
+    pkgs.nerd-fonts.heavy-data
+    pkgs.nerd-fonts.inconsolata
     pkgs.rcm
   ];
 
