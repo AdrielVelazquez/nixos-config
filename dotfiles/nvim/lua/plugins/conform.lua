f278e310159445aeb84f47ff5e8ac98988053f0a
Adriel Velazquez <adrielvelazquez@gmail.com>
2025-01-14 11:19:55 -0800

    sorting through how conform should format, and adding fzf-lua and trouble

--- dotfiles/nvim/lua/plugins/conform.lua
+++ dotfiles/nvim/lua/plugins/conform.lua
@@ -9,13 +9,18 @@ return {
         python = { 'black' },
         lua = { 'stylua' },
       },
+      format_on_save = {
+        lsp_fallback = true,
+        async = false,
+        timeout_ms = 500,
+      },
     }
 
-    vim.api.nvim_create_autocmd('BufWritePre', {
-      pattern = { '*.go', '*.nix', '*.py', '*.lua' },
-      callback = function()
-        conform.format { async = false, lsp_fallback = true }
-      end,
-    })
+    -- vim.api.nvim_create_autocmd('BufWritePre', {
+    --   pattern = { '*.go', '*.nix', '*.py', '*.lua' },
+    --   callback = function()
+    --     conform.format { async = false, lsp_fallback = true }
+    -- end,
+    -- })
   end,
 }
