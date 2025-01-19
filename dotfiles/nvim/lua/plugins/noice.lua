5d0a3b84442b631be6b4111e3e22d2601c437596
Adriel Velazquez <adrielvelazquez@gmail.com>
2025-01-19 15:16:41 -0800

    removing the signature from noice

--- dotfiles/nvim/lua/plugins/noice.lua
+++ dotfiles/nvim/lua/plugins/noice.lua
@@ -5,10 +5,8 @@ return -- lazy.nvim
   event = 'VeryLazy',
   opts = {
     lsp = {
-      override = {
-        ['vim.lsp.util.convert_input_to_markdown_lines'] = true,
-        ['vim.lsp.util.stylize_markdown'] = true,
-        ['cmp.entry.get_documentation'] = false,
+      signature = {
+        enabled = false,
       },
     },
     routes = {
