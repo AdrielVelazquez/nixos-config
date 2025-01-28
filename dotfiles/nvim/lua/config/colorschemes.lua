d8486069acd3db06e333e3b617465a009afbbc06
Adriel Velazquez <adrielvelazquez@gmail.com>
2025-01-28 14:38:55 -0800

    Adding more themes to test in general

--- dotfiles/nvim/lua/config/colorschemes.lua
+++ dotfiles/nvim/lua/config/colorschemes.lua
@@ -2,4 +2,6 @@
 -- vim.cmd("colorscheme rose-pine-dawn")
 -- vim.cmd("colorscheme rose-pine-moon")
 -- vim.cmd 'colorscheme tokyonight-night'
+--     vim.g.zenbones_darken_comments = 45
+--     vim.cmd.colorscheme('zenbones')
 vim.cmd 'colorscheme kanagawa-dragon'
--- dotfiles/nvim/lua/plugins/themes.lua
+++ dotfiles/nvim/lua/plugins/themes.lua
@@ -4,6 +4,20 @@ return {
   { 'folke/tokyonight.nvim', lazy = false, priority = 1000, opts = {} },
   { 'dgox16/oldworld.nvim', lazy = false, priority = 1000 },
   {
+    'zenbones-theme/zenbones.nvim',
+    -- Optionally install Lush. Allows for more configuration or extending the colorscheme
+    -- If you don't want to install lush, make sure to set g:zenbones_compat = 1
+    -- In Vim, compat mode is turned on as Lush only works in Neovim.
+    dependencies = 'rktjmp/lush.nvim',
+    lazy = false,
+    priority = 1000,
+    -- you can set set configuration options here
+    -- config = function()
+    --     vim.g.zenbones_darken_comments = 45
+    --     vim.cmd.colorscheme('zenbones')
+    -- end
+  },
+  {
     'rebelot/kanagawa.nvim',
     config = function()
       require('kanagawa').setup {
