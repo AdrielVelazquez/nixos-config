7f85fe28999b245896c5586bca3e8bff717eea18
Adriel Velazquez <adrielvelazquez@gmail.com>
2025-01-30 23:27:23 -0800

    Adding the offload script to steam

--- modules/system/steam.nix
+++ modules/system/steam.nix
@@ -1,6 +1,7 @@
 {
   lib,
   config,
+  pkgs,
   ...
 }:
 
@@ -8,6 +9,16 @@ with lib;
 
 let
   cfg = config.within.steam;
+
+  patchDesktop =
+    pkg: appName: from: to:
+    lib.hiPrio (
+      pkgs.runCommand "$patched-desktop-entry-for-${appName}" { } ''
+        mkdir -p $out/share/applications
+        sed 's#${from}#${to}#g' < ${pkg}/share/applications/${appName}.desktop > $out/share/applications/${appName}.desktop
+      ''
+    );
+  GPUOffloadApp = pkg: desktopName: patchDesktop pkg desktopName "^Exec=" "Exec=nvidia-offload ";
 in
 {
   options.within.steam.enable = mkEnableOption "Enables Steam Settings";
@@ -16,5 +27,9 @@ in
     programs.steam = {
       enable = true;
     };
+
+    environment.systemPackages = with pkgs; [
+      (GPUOffloadApp steam "steam")
+    ];
   };
 }
