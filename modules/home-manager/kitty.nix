eccd05aad310b3ef714e42020c3a1d61648fd1fd
Adriel Velazquez <adrielvelazquez@gmail.com>
2025-03-28 17:06:46 -0700

    better organized config for kitty

--- modules/home-manager/kitty.nix
+++ modules/home-manager/kitty.nix
@@ -21,18 +21,17 @@ in
 
     programs.kitty.enable = true;
     programs.kitty.shellIntegration.enableZshIntegration = true;
-    programs.kitty.settings = {
-      scrollback_pager_history_size = 60;
-
-      # Font Settings
+    # Font Settings
+    prorams.kitty.settings = {
       font_family = "Monaco";
       bold_font = "auto";
       italic_font = "auto";
       bold_italic_font = "auto";
       font_size = "16.0";
       font_features = "+zero +onum -liga +calt";
-
-      # Tab bar settings
+    };
+    # Tab bar settings
+    programs.kitty.settings = {
       tab_bar_edge = "top";
       tab_bar_align = "left";
       tab_bar_min_tabs = "1";
@@ -42,10 +41,24 @@ in
       active_tab_font_style = "bold";
       tab_bar_style = "custom";
       tab_title_template = "{f'{title[:30]}…' if title.rindex(title[-1]) + 1 > 30 else (title.center(6) if (title.rindex(title[-1]) + 1) % 2 == 0 else title.center(5))}";
+    };
 
+    # Linux settings
+    programs.kitty.settings = {
       # Linux Specific Options
       linux_display_server = "wayland";
+    };
 
+    # Mac settings
+    programs.kitty.settings = {
+      # Mac Specific Options
+      macos_option_as_alt = "yes";
+      macos_colorspace = "displayp3";
+    };
+
+    # General settings
+    programs.kitty.settings = {
+      scrollback_pager_history_size = 60;
       # Allow Remote
       # Following Plugins that require remote control
       # kitty-scrollback.nvim
@@ -60,14 +73,12 @@ in
       action_alias = "kitty_scrollback_nvim kitten $HOME/.local/share/nvim/lazy/kitty-scrollback.nvim/python/kitty_scrollback_nvim.py";
       hide_window_decorations = "no";
 
-      # Mac Specific Options
-      macos_option_as_alt = "yes";
-      macos_colorspace = "displayp3";
-
       # Nerd Fonts v3.2.0
       symbol_map = "U+e000-U+e00a,U+ea60-U+ebeb,U+e0a0-U+e0c8,U+e0ca,U+e0cc-U+e0d7,U+e200-U+e2a9,U+e300-U+e3e3,U+e5fa-U+e6b1,U+e700-U+e7c5,U+ed00-U+efc1,U+f000-U+f2ff,U+f000-U+f2e0,U+f300-U+f372,U+f400-U+f533,U+f0001-U+f1af0 Symbols Nerd Font Mono";
     };
+
     programs.kitty.themeFile = "MonaLisa";
+
     programs.kitty.keybindings = {
       "ctrl+shift+f" =
         "launch --type=overlay --stdin-source=@screen_scrollback /bin/sh -c \"${pkgs.fzf}/bin/fzf --no-sort --no-mouse --exact -i --tac | kitty +kitten clipboard\"";
