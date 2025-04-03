b13a57fbd68e35e289fb330b20995834bd50e7f4
Adriel Velazquez <adrielvelazquez@gmail.com>
2025-04-02 23:35:16 -0700

    Sleeker starship

--- modules/home-manager/starship.nix
+++ modules/home-manager/starship.nix
@@ -16,14 +16,29 @@ in
     programs.starship = {
       enable = true;
       settings = {
+        format = "$username$hostname$directory$git_branch$git_state$git_status$cmd_duration$line_break$python$character";
+
+        directory = {
+          style = "blue";
+        };
+
         character = {
           success_symbol = "[❯](purple)";
           error_symbol = "[❯](red)";
           vimcmd_symbol = "[❮](green)";
         };
+
+        git_branch = {
+          # Using ''...'' strings here too for consistency with format strings containing $
+          format = ''[$branch]($style)'';
+          style = "bright-black";
+        };
+
         git_status = {
-          format = "[[(*$conflicted$untracked$modified$staged$renamed$deleted)](218) ($ahead_behind$stashed)]($style)";
+          # Using ''...'' strings here too
+          format = ''[[(*$conflicted$untracked$modified$staged$renamed$deleted)](218) ($ahead_behind$stashed)]($style)'';
           style = "cyan";
+          # Ensure zero-width spaces are preserved if they were intentional in TOML
           conflicted = "​";
           untracked = "​";
           modified = "​";
@@ -32,18 +47,53 @@ in
           deleted = "​";
           stashed = "≡";
         };
-        git_branch = {
-          format = "[$branch]($style)";
+
+        git_state = {
+          # Using ''...'' strings to avoid needing to escape backslashes
+          format = ''\([$state( $progress_current/$progress_total)]($style)\) '';
           style = "bright-black";
         };
-        directory = {
-          style = "blue";
-        };
+
         cmd_duration = {
-          format = "[$duration]($style) ";
+          # Using ''...'' strings here too
+          format = ''[$duration]($style) '';
           style = "yellow";
         };
+
+        python = {
+          # Using ''...'' strings here too
+          format = ''[$virtualenv]($style) '';
+          style = "bright-black";
+        };
       };
+      # settings = {
+      #   character = {
+      #     success_symbol = "[❯](purple)";
+      #     error_symbol = "[❯](red)";
+      #     vimcmd_symbol = "[❮](green)";
+      #   };
+      #   git_status = {
+      #     format = "[[(*$conflicted$untracked$modified$staged$renamed$deleted)](218) ($ahead_behind$stashed)]($style)";
+      #     style = "cyan";
+      #     conflicted = "​";
+      #     untracked = "​";
+      #     modified = "​";
+      #     staged = "​";
+      #     renamed = "​";
+      #     deleted = "​";
+      #     stashed = "≡";
+      #   };
+      #   git_branch = {
+      #     format = "[$branch]($style)";
+      #     style = "bright-black";
+      #   };
+      #   directory = {
+      #     style = "blue";
+      #   };
+      #   cmd_duration = {
+      #     format = "[$duration]($style) ";
+      #     style = "yellow";
+      # };
     };
   };
 }
