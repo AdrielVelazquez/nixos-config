d9abdae1e646c0112b08f9f49596ff1d80fa2332
Adriel Velazquez <adrielvelazquez@gmail.com>
2025-06-27 09:50:18 -0700

    Adding the kerenl paams

--- modules/reddit/falcon/falcon-sensor.nix
+++ modules/reddit/falcon/falcon-sensor.nix
@@ -27,6 +27,28 @@ with lib;
     {
       within.sops.enable = true;
 
+      boot.kernel.extraConfig = ''
+        # Required BPF kernel options
+        BPF y
+        BPF_SYSCALL y
+        DEBUG_INFO_BTF y
+        TRACING y
+        KPROBE_EVENTS y
+        UPROBE_EVENTS y
+        BPF_JIT y
+        SECURITY y
+        KALLSYMS_ALL y
+        PROC_FS y
+        BSD_PROCESS_ACCT y
+        CGROUPS y
+        CGROUP_BPF y
+        DEBUG_INFO_BTF_MODULES y
+
+        # Options that can be compiled as modules
+        NET_CLS_BPF m
+        NET_CLS_ACT m
+        NET_SCH_INGRESS m
+      '';
       systemd.services.falcon-sensor = {
         enable = true;
         description = "CrowdStrike Falcon Sensor";
