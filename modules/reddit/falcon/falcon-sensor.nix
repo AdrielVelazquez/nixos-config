00134ff8bc096be821928a8e5fda4ef9319e0980
Adriel Velazquez <adrielvelazquez@gmail.com>
2025-06-27 22:46:54 -0700

    Using kernel 6.14 which runs in user mode

--- modules/reddit/falcon/falcon-sensor.nix
+++ modules/reddit/falcon/falcon-sensor.nix
@@ -26,57 +26,28 @@ with lib;
     in
     {
       within.sops.enable = true;
-      boot.kernelPatches = [
-        {
-          name = "enable-custom-bpf";
-          patch = null;
-          extraConfig = ''
-            BPF y
-            BPF_SYSCALL y
-            DEBUG_INFO_BTF y
-            TRACING y
-            KPROBE_EVENTS y
-            UPROBE_EVENTS y
-            BPF_JIT y
-            SECURITY y
-            KALLSYMS_ALL y
-            PROC_FS y
-            BSD_PROCESS_ACCT y
-            CGROUPS y
-            CGROUP_BPF y
-            DEBUG_INFO_BTF_MODULES y
-            NET_CLS_BPF y
-            NET_CLS_ACT y
-            NET_SCH_INGRESS y
-          '';
-        }
-      ];
       # boot.kernelPatches = [
-      #
       #   {
-      #     name = "bpf-and-tracing-config";
+      #     name = "enable-custom-bpf";
       #     patch = null;
       #     extraConfig = ''
-      #       # Required BPF kernel options
-      #       # CONFIG_BPF=y
-      #       # CONFIG_BPF_SYSCALL=y
-      #       # CONFIG_DEBUG_INFO_BTF=y
-      #       # CONFIG_TRACING=y
-      #       # CONFIG_KPROBE_EVENTS=y
-      #       # CONFIG_UPROBE_EVENTS=y
-      #       # CONFIG_BPF_JIT=y
-      #       # CONFIG_SECURITY=y
-      #       # CONFIG_KALLSYMS_ALL=y
-      #       # CONFIG_PROC_FS=y
-      #       # CONFIG_BSD_PROCESS_ACCT=y
-      #       # CONFIG_CGROUPS=y
-      #       # CONFIG_CGROUP_BPF=y
-      #       # CONFIG_DEBUG_INFO_BTF_MODULES=y
-      #
-      #       # Options that can be compiled as modules
-      #       CONFIG_NET_CLS_BPF=m
-      #       CONFIG_NET_CLS_ACT=m
-      #       CONFIG_NET_SCH_INGRESS=m
+      #       BPF y
+      #       BPF_SYSCALL y
+      #       DEBUG_INFO_BTF y
+      #       TRACING y
+      #       KPROBE_EVENTS y
+      #       UPROBE_EVENTS y
+      #       BPF_JIT y
+      #       SECURITY y
+      #       KALLSYMS_ALL y
+      #       PROC_FS y
+      #       BSD_PROCESS_ACCT y
+      #       CGROUPS y
+      #       CGROUP_BPF y
+      #       DEBUG_INFO_BTF_MODULES y
+      #       NET_CLS_BPF y
+      #       NET_CLS_ACT y
+      #       NET_SCH_INGRESS y
       #     '';
       #   }
       # ];
