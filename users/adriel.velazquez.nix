c27618769b4191d41e5749cee8d6aa1991da94d2
Adriel Velazquez <adrielvelazquez@gmail.com>
2025-07-03 09:37:24 -0700

    Don't install reddit defaults on mak

--- users/adriel.velazquez.nix
+++ users/adriel.velazquez.nix
@@ -3,7 +3,7 @@
 {
   imports = [
     ./../modules/home-manager/default.nix
-    ./../modules/reddit/default.nix
+    # ./../modules/reddit/default.nix
   ];
   # Home Manager needs a bit of information about you and the paths it should
   # manage.
