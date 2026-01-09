a4a1e600d36d2cc949e1d2a426a9372fe66c2976
Adriel Velazquez <AdrielVelazquez@gmail.com>
2026-01-09 15:05:22 -0500

    Adding the wrapped steam application back in since I can't get it to run on the gpu

--- modules/system/steam.nix
+++ modules/system/steam.nix
@@ -2,6 +2,7 @@
 {
   lib,
   config,
+  pkgs,
   ...
 }:
 
@@ -12,9 +13,20 @@ in
   options.local.steam.enable = lib.mkEnableOption "Enables Steam gaming platform";
 
   config = lib.mkIf cfg.enable {
-    programs.steam.enable = true;
-
-    # For NVIDIA offload per game, add to Steam launch options:
-    # nvidia-offload %command%
+    programs.steam = {
+      enable = true;
+      # Run Steam and all games on NVIDIA by default
+      # For games you want on AMD, add to launch options:
+      # env __NV_PRIME_RENDER_OFFLOAD= __GLX_VENDOR_LIBRARY_NAME= %command%
+      package = pkgs.steam.override {
+        extraProfile = ''
+          # Use NVIDIA for Steam and all games
+          export __NV_PRIME_RENDER_OFFLOAD=1
+          export __NV_PRIME_RENDER_OFFLOAD_PROVIDER=NVIDIA-G0
+          export __GLX_VENDOR_LIBRARY_NAME=nvidia
+          export __VK_LAYER_NV_optimus=NVIDIA_only
+        '';
+      };
+    };
   };
 }
