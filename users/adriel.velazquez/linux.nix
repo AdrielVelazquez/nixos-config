911bbfd90d0157e3d87a29d20ec937e16090f9e3
Adriel Velazquez <adrielvelazquez@gmail.com>
2026-02-26 16:42:51 -0500

    Adding obs studion to record my screen

--- users/adriel.velazquez/linux.nix
+++ users/adriel.velazquez/linux.nix
@@ -97,4 +97,16 @@
       line-numbers = true;
     };
   };
+  programs.obs-studio = {
+    enable = true;
+
+    plugins = with pkgs.obs-studio-plugins; [
+      wlrobs
+      obs-backgroundremoval
+      obs-pipewire-audio-capture
+      obs-vaapi # optional AMD hardware acceleration
+      obs-gstreamer
+      obs-vkcapture
+    ];
+  };
 }
