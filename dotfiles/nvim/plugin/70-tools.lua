09029c0e1bed3efd7ad8d955769fcc42c3a9a3e6
Adriel Velazquez <AdrielVelazquez@gmail.com>
2026-04-07 11:26:46 -0400

    Don't include the yank window

--- dotfiles/nvim/plugin/70-tools.lua
+++ dotfiles/nvim/plugin/70-tools.lua
@@ -1,4 +1,11 @@
 local pack = require 'config.pack'
+local kitty_scrollback_spec = pack.repo('AdrielVelazquez/kitty-scrollback.nvim', {
+  version = 'ripgrep-fzflua-file-on-disk',
+})
+
+-- Register the plugin spec up front so `vim.pack.update()` uses the fork source
+-- even though kitty-scrollback itself is configured lazily later on.
+pack.add({ kitty_scrollback_spec }, { load = false, confirm = false })
 
 local load_lint = function()
   pack.run_once('nvim-lint', function()
@@ -176,11 +183,21 @@ pack.proxy_command('Silicon', load_silicon, {
 
 local load_kitty_scrollback = function()
   pack.run_once('kitty-scrollback', function()
-    pack.add {
-      pack.repo 'mikesmithgh/kitty-scrollback.nvim',
+    require('kitty-scrollback').setup {
+      {
+        scrollback_buffer = {
+          tempfile = {
+            enabled = true,
+          },
+          paste_window = {
+            yank_register_enabled = false,
+          },
+          status_window = {
+            autoclose = false,
+          },
+        },
+      },
     }
-
-    require('kitty-scrollback').setup()
   end)
 end
 
