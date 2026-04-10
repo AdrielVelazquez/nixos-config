a8f42c396162c42304b864166cf246b291875c71
Adriel Velazquez <AdrielVelazquez@gmail.com>
2026-04-09 23:59:07 -0400

    Removing debugger since I barely use it.

--- dotfiles/nvim/plugin/91-tools-dap.lua
+++ dotfiles/nvim/plugin/91-tools-dap.lua
@@ -1,125 +1,125 @@
-local pack = require 'config.pack'
-
-local load_dap = function()
-  pack.run_once('dap-suite', function()
-    pack.add {
-      pack.repo 'mfussenegger/nvim-dap',
-      pack.repo 'rcarriga/nvim-dap-ui',
-      pack.repo 'nvim-neotest/nvim-nio',
-      pack.repo 'leoluz/nvim-dap-go',
-      pack.repo 'theHamsta/nvim-dap-virtual-text',
-    }
-
-    require('nvim-dap-virtual-text').setup {}
-    require('dap-go').setup {}
-
-    local dap = require 'dap'
-    local dapui = require 'dapui'
-
-    dapui.setup {}
-    dap.listeners.after.event_initialized.dapui_config = function()
-      dapui.open {}
-    end
-    dap.listeners.before.event_terminated.dapui_config = function()
-      dapui.close {}
-    end
-    dap.listeners.before.event_exited.dapui_config = function()
-      dapui.close {}
-    end
-  end)
-end
-
-vim.keymap.set('n', '<leader>dB', function()
-  load_dap()
-  require('dap').set_breakpoint(vim.fn.input 'Breakpoint condition: ')
-end, { desc = 'Breakpoint Condition' })
-
-vim.keymap.set('n', '<leader>db', function()
-  load_dap()
-  require('dap').toggle_breakpoint()
-end, { desc = 'Toggle Breakpoint' })
-
-vim.keymap.set('n', '<leader>dc', function()
-  load_dap()
-  require('dap').continue()
-end, { desc = 'Run/Continue' })
-
-vim.keymap.set('n', '<leader>da', function()
-  load_dap()
-  require('dap').continue { before = get_args }
-end, { desc = 'Run with Args' })
-
-vim.keymap.set('n', '<leader>dC', function()
-  load_dap()
-  require('dap').run_to_cursor()
-end, { desc = 'Run to Cursor' })
-
-vim.keymap.set('n', '<leader>dg', function()
-  load_dap()
-  require('dap').goto_()
-end, { desc = 'Go to Line (No Execute)' })
-
-vim.keymap.set('n', '<leader>di', function()
-  load_dap()
-  require('dap').step_into()
-end, { desc = 'Step Into' })
-
-vim.keymap.set('n', '<leader>dj', function()
-  load_dap()
-  require('dap').down()
-end, { desc = 'Down' })
-
-vim.keymap.set('n', '<leader>dk', function()
-  load_dap()
-  require('dap').up()
-end, { desc = 'Up' })
-
-vim.keymap.set('n', '<leader>dl', function()
-  load_dap()
-  require('dap').run_last()
-end, { desc = 'Run Last' })
-
-vim.keymap.set('n', '<leader>do', function()
-  load_dap()
-  require('dap').step_out()
-end, { desc = 'Step Out' })
-
-vim.keymap.set('n', '<leader>dO', function()
-  load_dap()
-  require('dap').step_over()
-end, { desc = 'Step Over' })
-
-vim.keymap.set('n', '<leader>dP', function()
-  load_dap()
-  require('dap').pause()
-end, { desc = 'Pause' })
-
-vim.keymap.set('n', '<leader>dr', function()
-  load_dap()
-  require('dap').repl.toggle()
-end, { desc = 'Toggle REPL' })
-
-vim.keymap.set('n', '<leader>ds', function()
-  load_dap()
-  require('dap').session()
-end, { desc = 'Session' })
-
-vim.keymap.set('n', '<leader>dt', function()
-  load_dap()
-  require('dap').terminate()
-end, { desc = 'Terminate' })
-
-vim.keymap.set('n', '<leader>dw', function()
-  load_dap()
-  require('dap.ui.widgets').hover()
-end, { desc = 'Widgets' })
-
-vim.keymap.set('n', '<leader>du', function()
-  load_dap()
-  require('dapui').toggle {}
-end, { desc = 'Dap UI' })
-
-vim.keymap.set({ 'n', 'v' }, '<leader>de', function()
-  load_dap()
-  require('dapui').eval()
-end, { desc = 'Eval' })
+-- local pack = require 'config.pack'
+--
+-- local load_dap = function()
+--   pack.run_once('dap-suite', function()
+--     pack.add {
+--       pack.repo 'mfussenegger/nvim-dap',
+--       pack.repo 'rcarriga/nvim-dap-ui',
+--       pack.repo 'nvim-neotest/nvim-nio',
+--       pack.repo 'leoluz/nvim-dap-go',
+--       pack.repo 'theHamsta/nvim-dap-virtual-text',
+--     }
+--
+--     require('nvim-dap-virtual-text').setup {}
+--     require('dap-go').setup {}
+--
+--     local dap = require 'dap'
+--     local dapui = require 'dapui'
+--
+--     dapui.setup {}
+--     dap.listeners.after.event_initialized.dapui_config = function()
+--       dapui.open {}
+--     end
+--     dap.listeners.before.event_terminated.dapui_config = function()
+--       dapui.close {}
+--     end
+--     dap.listeners.before.event_exited.dapui_config = function()
+--       dapui.close {}
+--     end
+--   end)
+-- end
+--
+-- vim.keymap.set('n', '<leader>dB', function()
+--   load_dap()
+--   require('dap').set_breakpoint(vim.fn.input 'Breakpoint condition: ')
+-- end, { desc = 'Breakpoint Condition' })
+--
+-- vim.keymap.set('n', '<leader>db', function()
+--   load_dap()
+--   require('dap').toggle_breakpoint()
+-- end, { desc = 'Toggle Breakpoint' })
+--
+-- vim.keymap.set('n', '<leader>dc', function()
+--   load_dap()
+--   require('dap').continue()
+-- end, { desc = 'Run/Continue' })
+--
+-- vim.keymap.set('n', '<leader>da', function()
+--   load_dap()
+--   require('dap').continue { before = get_args }
+-- end, { desc = 'Run with Args' })
+--
+-- vim.keymap.set('n', '<leader>dC', function()
+--   load_dap()
+--   require('dap').run_to_cursor()
+-- end, { desc = 'Run to Cursor' })
+--
+-- vim.keymap.set('n', '<leader>dg', function()
+--   load_dap()
+--   require('dap').goto_()
+-- end, { desc = 'Go to Line (No Execute)' })
+--
+-- vim.keymap.set('n', '<leader>di', function()
+--   load_dap()
+--   require('dap').step_into()
+-- end, { desc = 'Step Into' })
+--
+-- vim.keymap.set('n', '<leader>dj', function()
+--   load_dap()
+--   require('dap').down()
+-- end, { desc = 'Down' })
+--
+-- vim.keymap.set('n', '<leader>dk', function()
+--   load_dap()
+--   require('dap').up()
+-- end, { desc = 'Up' })
+--
+-- vim.keymap.set('n', '<leader>dl', function()
+--   load_dap()
+--   require('dap').run_last()
+-- end, { desc = 'Run Last' })
+--
+-- vim.keymap.set('n', '<leader>do', function()
+--   load_dap()
+--   require('dap').step_out()
+-- end, { desc = 'Step Out' })
+--
+-- vim.keymap.set('n', '<leader>dO', function()
+--   load_dap()
+--   require('dap').step_over()
+-- end, { desc = 'Step Over' })
+--
+-- vim.keymap.set('n', '<leader>dP', function()
+--   load_dap()
+--   require('dap').pause()
+-- end, { desc = 'Pause' })
+--
+-- vim.keymap.set('n', '<leader>dr', function()
+--   load_dap()
+--   require('dap').repl.toggle()
+-- end, { desc = 'Toggle REPL' })
+--
+-- vim.keymap.set('n', '<leader>ds', function()
+--   load_dap()
+--   require('dap').session()
+-- end, { desc = 'Session' })
+--
+-- vim.keymap.set('n', '<leader>dt', function()
+--   load_dap()
+--   require('dap').terminate()
+-- end, { desc = 'Terminate' })
+--
+-- vim.keymap.set('n', '<leader>dw', function()
+--   load_dap()
+--   require('dap.ui.widgets').hover()
+-- end, { desc = 'Widgets' })
+--
+-- vim.keymap.set('n', '<leader>du', function()
+--   load_dap()
+--   require('dapui').toggle {}
+-- end, { desc = 'Dap UI' })
+--
+-- vim.keymap.set({ 'n', 'v' }, '<leader>de', function()
+--   load_dap()
+--   require('dapui').eval()
+-- end, { desc = 'Eval' })
