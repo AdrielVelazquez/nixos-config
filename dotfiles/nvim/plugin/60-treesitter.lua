44c32cd75eae3c154e96f46231928a2eb8c7b5b3
Adriel Velazquez <AdrielVelazquez@gmail.com>
2026-04-14 12:56:55 -0400

    Revert "Removing treesitter completely"
    This reverts commit 49730ee891ddf1b76f0cfefa2770cf4a1449acf2.

--- dotfiles/nvim/plugin/60-treesitter.lua
+++ dotfiles/nvim/plugin/60-treesitter.lua
@@ -1,6 +1,9 @@
-local ts_textobjects = require 'config.treesitter_textobjects'
+local pack = require 'config.pack'
 
-ts_textobjects.setup()
+pack.add {
+  pack.repo('nvim-treesitter/nvim-treesitter', { version = 'main' }),
+  pack.repo('nvim-treesitter/nvim-treesitter-textobjects', { version = 'main' }),
+}
 
 vim.api.nvim_create_autocmd('FileType', {
   callback = function()
@@ -18,49 +21,51 @@ do
 
   for keymap, query in pairs(select_maps) do
     vim.keymap.set({ 'x', 'o' }, keymap, function()
-      ts_textobjects.select(query)
+      require('nvim-treesitter-textobjects.select').select_textobject(query, 'textobjects')
     end, { desc = 'Select ' .. query })
   end
 end
 
 do
+  local ts_move = require 'nvim-treesitter-textobjects.move'
+
   vim.keymap.set({ 'n', 'x', 'o' }, ']m', function()
-    ts_textobjects.goto_next_start '@function.outer'
+    ts_move.goto_next_start('@function.outer', 'textobjects')
   end, { desc = 'Next function start' })
 
   vim.keymap.set({ 'n', 'x', 'o' }, ']M', function()
-    ts_textobjects.goto_next_end '@function.outer'
+    ts_move.goto_next_end('@function.outer', 'textobjects')
   end, { desc = 'Next function end' })
 
   vim.keymap.set({ 'n', 'x', 'o' }, '[m', function()
-    ts_textobjects.goto_previous_start '@function.outer'
+    ts_move.goto_previous_start('@function.outer', 'textobjects')
   end, { desc = 'Previous function start' })
 
   vim.keymap.set({ 'n', 'x', 'o' }, '[M', function()
-    ts_textobjects.goto_previous_end '@function.outer'
+    ts_move.goto_previous_end('@function.outer', 'textobjects')
   end, { desc = 'Previous function end' })
 
   vim.keymap.set({ 'n', 'x', 'o' }, ']]', function()
-    ts_textobjects.goto_next_start '@class.outer'
+    ts_move.goto_next_start('@class.outer', 'textobjects')
   end, { desc = 'Next class start' })
 
   vim.keymap.set({ 'n', 'x', 'o' }, '][', function()
-    ts_textobjects.goto_next_end '@class.outer'
+    ts_move.goto_next_end('@class.outer', 'textobjects')
   end, { desc = 'Next class end' })
 
   vim.keymap.set({ 'n', 'x', 'o' }, '[[', function()
-    ts_textobjects.goto_previous_start '@class.outer'
+    ts_move.goto_previous_start('@class.outer', 'textobjects')
   end, { desc = 'Previous class start' })
 
   vim.keymap.set({ 'n', 'x', 'o' }, '[]', function()
-    ts_textobjects.goto_previous_end '@class.outer'
+    ts_move.goto_previous_end('@class.outer', 'textobjects')
   end, { desc = 'Previous class end' })
 
   vim.keymap.set({ 'n', 'x', 'o' }, ']d', function()
-    ts_textobjects.goto_next_start '@conditional.outer'
+    ts_move.goto_next_start('@conditional.outer', 'textobjects')
   end, { desc = 'Next conditional' })
 
   vim.keymap.set({ 'n', 'x', 'o' }, '[d', function()
-    ts_textobjects.goto_previous_start '@conditional.outer'
+    ts_move.goto_previous_start('@conditional.outer', 'textobjects')
   end, { desc = 'Previous conditional' })
 end
