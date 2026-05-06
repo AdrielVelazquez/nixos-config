fd3e6ba4ed446ce8fc7c3c1647413596ede97847
Adriel Velazquez <AdrielVelazquez@gmail.com>
2026-05-05 23:58:11 -0400

    Adding dolphin

--- users/adriel/default.nix
+++ users/adriel/default.nix
@@ -2,7 +2,7 @@
 #
 # Personal `adriel` user, razer14 flavor. Adds niri config tied to the
 # razer14 hybrid GPU layout. The reusable bits live in ./common.nix.
-{ ... }:
+{ pkgs, ... }:
 
 {
   imports = [
@@ -17,5 +17,13 @@
     hasDgpu = true;
   };
 
+  local.web-mime-defaults.fileManager = "org.kde.dolphin.desktop";
+
   local.zoom.enable = true;
+
+  home.packages = with pkgs; [
+    kdePackages.dolphin
+    kdePackages.dolphin-plugins
+    kdePackages.kio-extras
+  ];
 }
